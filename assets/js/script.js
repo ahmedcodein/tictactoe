c0a8b8bedef8292276100be9f0ea75a5d0fce4cd
Ahmed Al-Mudhafar <ahmed.almudhafar@gmail.com>
2024-04-15 12:50:47 +0000

    Add code to declare the first player character

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,12 +11,35 @@ document.addEventListener("DOMContentLoaded", function () {
     let roundReset = document.getElementById('round-reset'); // declare a round reset variable
     let gameReset = document.getElementById('game-reset'); // declare a resut button variable
 
+    initiateTheGame(); // Game is loaded and is waiting the user to choose either of the players to strart the game with
+
+    /**This function initiate the Game once the DOM is loaded.
+     * It waits then the user to click on one of the users to start the game.
+     */
     function initiateTheGame() {
 
+        // aPlayerClickedEvent variable is a gating mechanismim to prevent new player selection
+        let aPlayerClickedEvent = false;
+
+        startTheGame(players,aPlayerClickedEvent, function(firstPlayer) {
+
+        });
+
     }
-
-    function startTheGame() {
-
+    /**This function listens to the players buttons. Once either of the players clicked
+     * it declars the first player character.
+     */
+    function startTheGame(players,aPlayerClickedEvent, callback) {
+        // Check which players buttons was fired
+        for (let i = 0; i < players.length; i++) {
+            players[i].addEventListener('click', function() {
+                if (!aPlayerClickedEvent) {
+                    // Prevents any more player's selection
+                    aPlayerClickedEvent = true;
+                    console.log(players[i].innerText)
+                }
+            });
+        }
     }
 
     function playersDeclaration(){
