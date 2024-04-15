7f4f31752e38a691e0fd72ca5511fbf729fa9a63
Ahmed Al-Mudhafar <ahmed.almudhafar@gmail.com>
2024-04-15 11:17:18 +0000

    Add event listener to the dom

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+console.clear()
+// Allow the DOM to be loaded before starting the game
+// This line of code is adopted from JS workthrough project
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("Page is loaded")
+})
