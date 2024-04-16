f95e93a8d5a7998d5f47d05f38e32cca79c37071
Ahmed Al-Mudhafar <ahmed.almudhafar@gmail.com>
2024-04-16 01:33:18 +0000

    Clean up the js file, correct all the typos

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,10 @@ console.clear()
 document.addEventListener("DOMContentLoaded", function () {
     console.log("Page is loaded")
 
-    // Declare inputs to the game, connect them to the DOM where approperiate
+    // Declare inputs to the game, connect them to the DOM where appropriate
     let players = Array.from(document.getElementsByClassName('players')); // Convert to an array
     let Spots = Array.from(document.getElementsByClassName('spot')); // Convert to an array
-    let playingSpots = Array(Spots.length).fill(null); // Create an empty
+    let playingSpots = Array(Spots.length).fill(null); // Create an empty array
     let roundReset = document.getElementById('round-reset'); // declare a round reset variable
     let gameReset = document.getElementById('game-reset'); // declare a resut button variable
 
@@ -15,26 +15,27 @@ document.addEventListener("DOMContentLoaded", function () {
     roundReset.addEventListener('click', resetRoundFunction);
     gameReset.addEventListener('click', resetGameFunction);
 
-    initiateTheGame(); // Game is loaded and is waiting the user to choose either of the players to strart the game with
+    initiateTheGame(); // Game is loaded and is waiting the user to choose either of the players to start the game with
 
     /**This function initiate the Game once the DOM is loaded.
      * It waits then the user to click on one of the users to start the game.
      */
     function initiateTheGame() {
 
-        // aPlayerClickedEvent variable is a gating mechanismim to prevent new player selection
+        // aPlayerClickedEvent variable is a gating mechanism to prevent new player selection
         let aPlayerClickedEvent = false;
         let count = -1; // Used to count the number of clicks when the filltheSpots function is called
         startTheGame(players, aPlayerClickedEvent, function (firstPlayer) {
             let secondPlayer = playersDeclaration(firstPlayer);
             fillTheSpots(count, firstPlayer, secondPlayer, Spots, playingSpots, function (filledInSpots) {
+                gameResultEvaluation(filledInSpots);
 
             });
         });
 
     }
     /**This function listens to the players buttons. Once either of the players clicked
-     * it declars the first player character.
+     * it declares the first player character.
      */
     function startTheGame(players, aPlayerClickedEvent, callback) {
         // Check which players buttons was fired
@@ -48,20 +49,18 @@ document.addEventListener("DOMContentLoaded", function () {
             });
         }
     }
-    /** This function declare the second player by observing the charactor of the first player
-     * It also announce the character turn on the dashboard once, the first charater is choosen
+    /** This function declares the second player by observing the character of the first player
+     * It also announce the character turn on the dashboard once, the first character is chosen
      */
     function playersDeclaration(firstPlayer) {
-        // if O character is clicked, then O is the first player and X is the second player and vise versa
+        // if O character is clicked, then O is the first player and X is the second player and vice versa
         firstPlayer === "O" ? secondPlayer = "X" : secondPlayer = "O";
         document.getElementById("dash-board").innerText = `It is ${firstPlayer}'s turn`;
         return secondPlayer
     }
 
-    function fillTheSpots(count, firstPlayer, secondPlayer, gameSpots, playingSpots, callback) {
-        console.log(count);
+    function fillTheSpots(count, firstPlayer, secondPlayer, Spots, playingSpots, callback) {
 
-        // declare an array of 
         let listenerStorage = [];
 
         for (let i = 0; i < Spots.length; i++) {
@@ -88,8 +87,37 @@ document.addEventListener("DOMContentLoaded", function () {
         listenedToCharactor = listenerStorage;
     }
 
-    function gameResultEvaluation() {
+    function gameResultEvaluation(filledInSpots) {
+        let winner
+        let winnerFound = false;
+        const winningCombinations = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
 
+        for (let i = 0; i < winningCombinations.length; i++) {
+            let [a, b, c] = winningCombinations[i];
+            let winningConditon = (filledInSpots[a] !== null && (filledInSpots[a] === filledInSpots[b] && filledInSpots[a] === filledInSpots[c]));
+            if (winningConditon) {
+                winner = filledInSpots[a];
+                winnerFound = true;
+            }
+        }
+
+        let drawCondition = filledInSpots.filter(item => item !== null).length === filledInSpots.length;
+        if (!winnerFound && drawCondition) {
+            console.log("It is draw")
+        }
+
+        if (typeof winner === "string") {
+            console.log(winner)
+        }
     }
 
     function resultAnnoucement() {
@@ -106,13 +134,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function resetGameFunction() {
 
-        // Reset all the manupulated inputs to their defult conditions
+
+        console.clear()
+        // Reset all the manipulated inputs to their default conditions
         for (let i = 0; i < Spots.length; i++) {
             // Remove event listeners
             Spots[i].removeEventListener('click', listenedToCharactor[i]);
             Spots[i].innerText = '';
         }
-        document.getElementById("dash-board").innerText = "Choose either of the character to start the game!";
+        document.getElementById("dash-board").innerText = "Choose either of the characters to start the game!";
         playingSpots.fill(null)
         alert("The game is reset")
         // re initiate the game on the default inputs
